750ca389bc63a7fa9215f623ffc69f9f7557c602
A-freire <bexas94@gmail.com>
2026-04-07 14:43:42 +0200

    8

--- Final/ex00/Xnote/entrypoint.sh
+++ Final/ex00/Xnote/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+mkdir -p /app/tmp/pids /app/log /app/tmp /app/app/assets/images
 rm -f /app/tmp/pids/server.pid
+
+if [[ "$*" == *"rails server"* ]]; then
+  bundle exec rails db:prepare
+fi
 
 exec "$@"
